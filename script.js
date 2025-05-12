6781bb8492139ada1e7a91f75fc86f1b798f66e0
KajieAbenir <brentabenir.2023@gmail.com>
2025-05-12 20:40:37 +0800

    with copilot response. will edit next time

---
+++ script.js
@@ -0,0 +1,31 @@
+// addButton.addEventListener('click', function() {
+//     // Create a new <div> element
+//     const newDiv = document.createElement('div');
+
+//     // Optionally, add some text content or attributes to the <div>
+//     newDiv.textContent = 'This is a new div added by JavaScript!';
+//     newDiv.style.border = '2px solid black';
+//     newDiv.style.padding = '10px';
+//     newDiv.style.marginTop = '10px';
+
+//     // Append the new <div> to the body or another container
+//     document.body.appendChild(newDiv);
+// });
+
+// global variables
+
+// function section
+
+function addConversation (msgString,align){
+
+}
+
+function changeBG () {
+    document.getElementById('background-pic').style.backgroundImage = 'url("sample2.jpg")'
+    
+    console.log('Background changed at: ' + new Date().toString());
+}
+
+// event listeners
+
+
